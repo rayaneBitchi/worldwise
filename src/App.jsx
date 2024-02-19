0db49da5ac4e26cb8dface3f6623a8f3e5ee5606
Rayane Bitchi <rayamevoltaire@gmail.com>
2024-02-19 00:51:53 -0500

    WorldWise app done

--- src/App.jsx
+++ src/App.jsx
@@ -11,22 +11,12 @@ import City from "./components/City";
 import Form from "./components/Form";
 import SpinnerFullPage from "./components/SpinnerFullPage";
 
-// import Product from "./pages/Product";
-// import Pricing from "./pages/Pricing";
-// import PageNotFound from "./pages/PageNotFound";
-// import AppLayout from "./pages/AppLayout";
-// import Homepage from "./pages/Homepage";
-// import Login from "./pages/Login";
-
 const Homepage = lazy(() => import("./pages/Homepage"));
 const Product = lazy(() => import("./pages/Product"));
 const Pricing = lazy(() => import("./pages/Pricing"));
 const PageNotFound = lazy(() => import("./pages/PageNotFound"));
 const AppLayout = lazy(() => import("./pages/AppLayout"));
 const Login = lazy(() => import("./pages/Login"));
-
-// dist/assets/index-e0a91124.css   29.91 kB │ gzip:   5.11 kB
-// dist/assets/index-620cd8de.js   510.87 kB │ gzip: 147.25 kB
 
 function App() {
   return (
